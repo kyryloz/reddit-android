d7726ea908222e9d2813448bdce51ad0578b16a6
Kyrylo Zapylaiev <zapylaev@gmail.com>
2017-09-13 00:46:25 +0300

    Init project

---
+++ app/src/main/java/com/robotnec/reddit/RedditActivity.java
@@ -0,0 +1,13 @@
+package com.robotnec.reddit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RedditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reddit);
+    }
+}
