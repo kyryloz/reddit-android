2c449fd73add76eae982ea92a8e10ee551fd49d1
Kyrylo Zapylaiev <zapylaev@gmail.com>
2017-09-16 21:28:50 +0300

    Request storage permissions

--- app/src/main/java/com/robotnec/reddit/ui/activity/ImageViewerActivity.java
+++ app/src/main/java/com/robotnec/reddit/ui/activity/ImageViewerActivity.java
@@ -1,12 +1,21 @@
 package com.robotnec.reddit.ui.activity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.single.BasePermissionListener;
+import com.karumi.dexter.listener.single.CompositePermissionListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.karumi.dexter.listener.single.SnackbarOnDeniedPermissionListener;
 import com.robotnec.reddit.R;
 import com.squareup.picasso.Picasso;
 
@@ -21,6 +30,11 @@ public class ImageViewerActivity extends AppCompatActivity {
     @BindView(R.id.imageView)
     ImageView imageView;
 
+    @BindView(R.id.rootView)
+    ViewGroup rootView;
+
+    private String imageUrl;
+
     public static Intent createIntent(Context context, String imageUrl) {
         Intent intent = new Intent(context, ImageViewerActivity.class);
         intent.putExtra(EXTRA_IMAGE_URL, imageUrl);
@@ -33,13 +47,36 @@ public class ImageViewerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image_viewer);
         ButterKnife.bind(this);
 
+        imageUrl = getIntent().getStringExtra(EXTRA_IMAGE_URL);
+
         Picasso.with(this)
-                .load(getIntent().getStringExtra(EXTRA_IMAGE_URL))
+                .load(imageUrl)
                 .into(imageView);
     }
 
     @OnClick(R.id.saveButton)
     void onSaveButtonClick() {
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(createPermissionListener())
+                .check();
+    }
+
+    private PermissionListener createPermissionListener() {
+        return new CompositePermissionListener(
+                SnackbarOnDeniedPermissionListener.Builder
+                        .with(rootView, getString(R.string.access_storage_permission_rationale))
+                        .withDuration(Snackbar.LENGTH_INDEFINITE)
+                        .withOpenSettingsButton(getString(R.string.settings)).build(),
+                new BasePermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        saveImage();
+                    }
+                });
+    }
+
+    private void saveImage() {
         // TODO
     }
 }
