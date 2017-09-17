578be04fe2975a8aa173b726f415e5c5f7cbb737
Kyrylo Zapylaiev <zapylaev@gmail.com>
2017-09-17 22:56:13 +0300

    Impl pending intent to open downloaded image

--- app/src/main/java/com/robotnec/reddit/core/androidservice/ImageDownloadingAndroidService.java
+++ app/src/main/java/com/robotnec/reddit/core/androidservice/ImageDownloadingAndroidService.java
@@ -1,6 +1,7 @@
 package com.robotnec.reddit.core.androidservice;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -55,13 +56,12 @@ public class ImageDownloadingAndroidService extends JobIntentService {
         String imageUrl = intent.getStringExtra(EXTRA_IMAGE_URL);
         logger.debug("Loading image '{}'...", imageUrl);
 
-        startNotification();
-
         try {
-            Bitmap bitmap = picasso.load(imageUrl).get();
-            logger.debug("Bitmap '{}' loaded, saving...", imageUrl);
             File file = createOutputImageFile(imageUrl);
             if (file != null) {
+                startNotification();
+                Bitmap bitmap = picasso.load(imageUrl).get();
+                logger.debug("Bitmap '{}' loaded, saving...", imageUrl);
                 saveToFile(bitmap, file);
             } else {
                 logger.error("Failed to create image file");
@@ -87,7 +87,7 @@ public class ImageDownloadingAndroidService extends JobIntentService {
         mediaScanIntent.setData(contentUri);
         sendBroadcast(mediaScanIntent);
 
-        stopNotification();
+        stopNotification(contentUri);
     }
 
     @Nullable
@@ -120,9 +120,13 @@ public class ImageDownloadingAndroidService extends JobIntentService {
         notifyManager.notify(0, notificationBuilder.build());
     }
 
-    private void stopNotification() {
+    private void stopNotification(Uri imageUri) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(imageUri, "image/jpeg");
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
         notificationBuilder.setContentText(getString(R.string.download_complete));
         notificationBuilder.setProgress(0, 0, false);
+        notificationBuilder.setContentIntent(contentIntent);
         notifyManager.notify(0, notificationBuilder.build());
     }
 }
